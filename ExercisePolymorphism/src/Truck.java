3652b07ab8b945fbb8dc0a61f46e583791f6c06a
AhmedAlharbi15 <159686025+AhmedAlharbi15@users.noreply.github.com>
2024-03-03 10:17:33 +0300

    Update Truck.java

--- ExercisePolymorphism/src/Truck.java
+++ ExercisePolymorphism/src/Truck.java
@@ -3,6 +3,26 @@ class Truck implements Vehicle {
     private int weeks;
     private final double weeklyRate = 500.0;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeeks() {
+        return weeks;
+    }
+
+    public void setWeeks(int weeks) {
+        this.weeks = weeks;
+    }
+
+    public double getWeeklyRate() {
+        return weeklyRate;
+    }
+
     public Truck(String type, int weeks) {
         this.type = type;
         this.weeks = weeks;
